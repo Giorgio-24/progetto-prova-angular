5f81a3ea34d7dc27aad34d1cfbfbce488f22c3a1
Giorgio-24 <cipuandrei@libero.it>
2022-02-25 09:59:09 +0100

    main dashboards for users added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './modules/admin/admin.module'
+import { DoctorModule } from './modules/doctor/doctor.module'
+import { UserModule } from './modules/user/user.module'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,6 +15,8 @@ import { AdminModule } from './modules/admin/admin.module'
     BrowserModule,
     AppRoutingModule,
     AdminModule,
+    DoctorModule,
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
